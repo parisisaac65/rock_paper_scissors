4b414859197f7814488f40bff9eaea11eddf6993
Paris Isaac <parisisaac65@gmail.com>
2022-03-07 15:56:57 -0500

    Add three buttons - rock, paper, scissors

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function computerPlay(){
     const options = ['rock', 'paper', 'scissors']; 
     return options[Math.floor(Math.random() * options.length)]; 
 }
+let computerSelection = computerPlay();
 
 //declare global variables playerWin and playerTie 
 let playerWin; 
@@ -42,12 +43,12 @@ function playRound(playerSelection, computerSelection){
     return playerWin; 
 }
 
-function isValid(playerSelection){
-    if (playerSelection === null || playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors'){
-        return false;
-    }
-    return true; 
-}
+// function isValid(playerSelection){
+//     if (playerSelection === null || playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors'){
+//         return false;
+//     }
+//     return true; 
+// }
 
 function showMessage(){
     if (playerTie) {
@@ -58,52 +59,75 @@ function showMessage(){
         console.log(`You Lose! ${computerSelection} beat(s) ${playerSelection}`);}
 }
 
+//Create three buttons, one for rock, paper, and scissors
+const rockBtn = document.querySelector('.rockBtn');
+rockBtn.addEventListener('click', () => {
+    playerSelection = 'rock'; 
+    playRound(playerSelection, computerSelection);
+    showMessage();
+}); 
 
-// function declaration to play a 5 round game that keeps score and reports winner or loser at end 
-function game(){
-    // create two score variables: one for computer and one for user 
-    let playerScore = 0; 
-    let computerScore = 0; 
-    // use for loop to play 5 rounds 
-    for (let i = 1; i < 6; i++){
-    //call playRound function inside for loop 
-        playerSelection = prompt('Please enter rock, paper, scissors to play: ');
-        if (isValid(playerSelection)) {
-            playerSelection = playerSelection;
-        }
-        else {
-            playerSelection = prompt('INVALID: Please enter rock, paper, scissors to play: ');
-        }
-        computerSelection = computerPlay();
-        playRound(playerSelection, computerSelection);
-        showMessage(); 
+const paperBtn = document.querySelector('.paperBtn'); 
+paperBtn.addEventListener('click', () => {
+    playerSelection = 'paper';
+    playRound(playerSelection, computerSelection); 
+    showMessage();
+}); 
+
+const scissorsBtn  = document.querySelector ('.scissorsBtn');
+scissorsBtn.addEventListener('click', () => {
+    playerSelection = 'scissors';
+    playRound(playerSelection, computerSelection); 
+    showMessage();
+}); 
+
+
+
+// // function declaration to play a 5 round game that keeps score and reports winner or loser at end 
+// function game(){
+//     // create two score variables: one for computer and one for user 
+//     let playerScore = 0; 
+//     let computerScore = 0; 
+//     // use for loop to play 5 rounds 
+//     for (let i = 1; i < 6; i++){
+//     //call playRound function inside for loop 
+//         playerSelection = prompt('Please enter rock, paper, scissors to play: ');
+//         if (isValid(playerSelection)) {
+//             playerSelection = playerSelection;
+//         }
+//         else {
+//             playerSelection = prompt('INVALID: Please enter rock, paper, scissors to play: ');
+//         }
+//         computerSelection = computerPlay();
+//         playRound(playerSelection, computerSelection);
+//         showMessage(); 
  
-    //if player wins, increment the playerScore by 1 
-        if (playerWin){
-            playerScore++;
-    //if computer wins, increment the computerScore by 1 
-        } else if (playerWin === false && playerTie === false) {
-            computerScore++; 
-        } 
-    //display the results of each round
-        console.log(`Round: ${i} Player Score: ${playerScore}   Computer Score: ${computerScore}`); 
-    }
+//     //if player wins, increment the playerScore by 1 
+//         if (playerWin){
+//             playerScore++;
+//     //if computer wins, increment the computerScore by 1 
+//         } else if (playerWin === false && playerTie === false) {
+//             computerScore++; 
+//         } 
+//     //display the results of each round
+//         console.log(`Round: ${i} Player Score: ${playerScore}   Computer Score: ${computerScore}`); 
+//     }
 
-    //display the results of the final winner 
-    if (playerScore > computerScore){
-        console.log('Final Winner: YOU!');
-    }
-    else if (playerScore < computerScore){
-        console.log('Final Winner: Computer!');
-    }
-    else if (playerScore === computerScore){
-        console.log('No Winner. TIED');
-    }
-}
+//     //display the results of the final winner 
+//     if (playerScore > computerScore){
+//         console.log('Final Winner: YOU!');
+//     }
+//     else if (playerScore < computerScore){
+//         console.log('Final Winner: Computer!');
+//     }
+//     else if (playerScore === computerScore){
+//         console.log('No Winner. TIED');
+//     }
+//}
 
 
 
 // Ask the user to input either rock, paper, or scissors 
 // let playerSelection = prompt('Please enter rock, paper, scissors to play: ');
-// let computerSelection = computerPlay();
-console.log(game()); 
+
+//console.log(game()); 
